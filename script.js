1662a9afe71395dca07b1308bbd2b3f9a8500925
huans209 <huans209@newschool.edu>
2023-11-22 00:14:11 -0500

    11/22

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $( document ).ready(function() {
     console.log( "ready" );
     $(".flexbox1").hover(function(){
-        $(".displayartist").children().text(`${$(this).attr('title')}`)
+        $(".displayartist").children().text(`${$(this).attr('id1')}`)
         console.log("hover1")
-        $(".displaytrack").children().text(`${$(this).attr('id')}`)
+        $(".displaytrack").children().text(`${$(this).attr('id2')}`)
         console.log("hover2")
       });
 
