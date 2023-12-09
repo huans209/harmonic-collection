1cf2010b2345ad51a5a43f499f96dd1e0243b2cc
huans209 <huans209@newschool.edu>
2023-12-08 20:34:22 -0500

    12/8

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 $( document ).ready(function() {
     console.log( "ready" );
-    $(".flexbox1").hover(function(){
-        $(".displayartist").children().text(`${$(this).attr('data-1')}`)
-        console.log("hoverartist")
-        $(".displaytrack").children().text(`${$(this).attr('data-2')}`)
-        console.log("hovertrack")
-        $(".displaydash").children().text(`-`)
-        console.log("hoverdash")
-     });
-     $(".flexbox1").click(function(){
-        $(this).toggleClass("flexdot")
-        // $("#flexdot").animate({
-        //     opacity: '1',},1000,function(){
-        // });
-        console.log("clickbefore")
-    })
-    $(".buttonpop").hover(function(){
-        $(".gradindex").toggleClass("gradpop")
-     });
-     $(".buttonalt").hover(function(){
-        $(".gradindex").toggleClass("gradalt")
-     });
-     $(".buttonele").hover(function(){
-        $(".gradindex").toggleClass("gradele")
-     });
-     $(".buttonjpop").hover(function(){
-        $(".gradindex").toggleClass("gradjpop")
-     });
-     $(".buttonost").hover(function(){
-        $(".gradindex").toggleClass("gradost")
-     });
+   $(".flexbox1").hover(function(){
+      $(".displayartist").children().text(`${$(this).attr('data-1')}`)
+      console.log("hoverartist")
+      $(".displaytrack").children().text(`${$(this).attr('data-2')}`)
+      console.log("hovertrack")
+      $(".displaydash").children().text(`-`)
+      console.log("hoverdash")
+   });
+
+   $(".flexbox1").click(function(){
+      $(this).toggleClass("flexdot")
+      console.log("clickbefore")
+   })
+
+   $(".buttonpop").hover(function(){
+      $(".gradindex").toggleClass("gradpop")
+   });
+   $(".buttonalt").hover(function(){
+      $(".gradindex").toggleClass("gradalt")
+   });
+   $(".buttonele").hover(function(){
+      $(".gradindex").toggleClass("gradele")
+   });
+   $(".buttonjpop").hover(function(){
+      $(".gradindex").toggleClass("gradjpop")
+   });
+   $(".buttonost").hover(function(){
+      $(".gradindex").toggleClass("gradost")
+   });
+
+   var chord = $("#chordswell")[0];
+   $("#flexbox1").mouseenter (function() {
+      chord.play();
+   });
+   console.log("hoveraudio")
 });
